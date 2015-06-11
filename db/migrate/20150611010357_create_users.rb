feb5cf75bd32efbf4adb64d4fad9903a17eb0233
Clopez <copez.laros@gmail.com>
2015-06-10 18:07:58 -0700

    Added User Model

---
+++ db/migrate/20150611010357_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :email
+      t.boolean :is_admin
+      t.string :password_digest
+      t.integer :location_id
+
+      t.timestamps null: false
+    end
+  end
+end
