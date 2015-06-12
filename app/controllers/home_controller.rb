f6f6a2f55632f18e067b1a982866cfdda6d7c6a9
g2gonzalez <gonzalezrgerardo@gmail.com>
2015-06-11 18:48:43 -0700

    Added basic styling and navbar

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,4 @@
 class HomeController < ApplicationController
-
 	def index
     if session[:is_admin] == true
       @message = "Welcome Admin"
@@ -7,5 +6,17 @@ class HomeController < ApplicationController
       @message = "Welcome User"
     end
 	end
-
 end
+
+=begin
+
+locations:
+apt_name | address | city | state | zip
+
+bills:
+apt_name | amount | due date
+
+=end
+
+
+
