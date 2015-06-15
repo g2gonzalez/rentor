27071694850aef40e3a2bf7b8ac558e4fff72fba
g2gonzalez <gonzalezrgerardo@gmail.com>
2015-06-14 20:02:01 -0700

    Added validations to the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ActiveRecord::Base
-  has_secure_password
+	#validate that a name is entered
+	validates :name, :email, presence: true
+	#validates if the email has a correct format
+	validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+	#validate that passwords match and fields are not blank
+	has_secure_password
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
 Rails.application.routes.draw do
 	root 'sessions#index'
 
-  	get  		'/home'     		=> 		'home#index'
+  	get  	'/home'     		=> 		'home#index'
 
-  	get  		'/landlord' 		=> 		'landlords#index'
-  	get  		'/renter'   		=> 		'renters#index'
+  	get  	'/landlord' 		=> 		'landlords#index'
+  	get  	'/renter'   		=> 		'renters#index'
 
-  	get  		'/login'    			=> 		'sessions#new'
-  	post 		'/login'    			=> 		'sessions#create'
-  	get  		'/logout'   		=>		 'sessions#destroy'
+  	get  	'/login'    		=> 		'sessions#new'
+  	post 	'/login'    		=> 		'sessions#create'
+  	get  	'/logout'   		=>		 'sessions#destroy'
 
-  	get  		'/signup'   		=> 		'users#new'
-  	post 		'/users'    			=> 		'users#create'
+  	get  	'/signup'   		=> 		'users#new'
+  	post 	'/users'    		=> 		'users#create'
 end
