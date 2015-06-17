e3ed1e1e8ef4d2a0f97e1740e9010c7b6a5a0df3
Clopez <copez.laros@gmail.com>
2015-06-16 20:43:25 -0700

    Pulled Corrections, Completed Locations Model

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -4,7 +4,20 @@ class LocationsController < ApplicationController
   end
 
   def create
+    @location = Location.new(location_params)
 
+    if @location.save
+      redirect_to home_path
+    else
+      render :new
+    end
+
+  end
+
+
+private
+  def location_params
+    params.require(:location).permit(:apt_name, :address, :city, :state, :zip, :user_id)
   end
 
 end
